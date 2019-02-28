faa60d38ba65a0dd2af412b220d08f69175e28e1
NimrodM <nimrodm@way2vat.com>
2019-02-28 22:34:22 +0200

    refactor

--- src/Solver.py
+++ src/Solver.py
@@ -12,7 +12,26 @@ INPUTS = {'a': '../inputs/a_example.txt',
           'd': '../inputs/d_pet_pictures.txt',
           'e': '../inputs/e_shiny_selfies.txt'}
 
-from src.Parser import parseIn, parseOut
+
+def parseIn(path):
+    with open(path, 'r') as fp:
+        numPhotos = int(fp.readline().strip().split()[0])
+        photos    = []
+        for line in fp:
+            orientation, numTags, *tags = line.strip().split()
+            photos.append({'orient':  orientation,
+                           'numTags': int(numTags),
+                           'tags':    tags,
+                           'id':      str(len(photos))})
+
+    return numPhotos, photos
+
+def parseOut(path, slideshow):
+    print(slideshow)
+    with open(path, 'w') as fp:
+        fp.write('{}\n'.format(len(slideshow)))
+        [fp.write('{}\n'.format(slide)) for slide in slideshow]
+
 
 def plotHist(lst, title):
     max = np.max(lst)
