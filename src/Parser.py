182a9a83e9b3c1e1c1e18e30691fd8176a4d9011
NimrodM <nimrodm@way2vat.com>
2019-02-28 19:57:03 +0200

    parsers

--- src/Parser.py
+++ src/Parser.py
@@ -3,19 +3,17 @@ import numpy as np
 
 def parseIn(path):
     with open(path, 'r') as fp:
-        rows, cols, numCars, numRides, bonus, maxTime = fp.readline().strip().split()
-        rides = []
+        numPhotos = int(fp.readline().strip().split()[0])
+        photos    = []
         for line in fp:
-            a, b, x, y, s, f = line.strip().split()
-            rides.append({'startPoint' : (int(a), int(b)),
-                          'endPoint'   : (int(x), int(y)),
-                          'startTime'  : int(s),
-                          'endTime'    : int(f),
-                          'rideNum'    : len(rides)})
+            orientation, numTags, *tags = line.strip().split()
+            photos.append({'orientation': orientation,
+                           'numTags':     numTags,
+                           'tags':        tags})
 
-    return rides, int(rows), int(cols), int(numCars), int(numRides), int(bonus), int(maxTime)
+    return numPhotos, photos
 
-def parseOut(path, schedule):
+def parseOut(path, slideshow):
     with open(path, 'w') as fp:
-        for car in schedule:
-            fp.write('{numRides} {rideIds}\n'.format(numRides=len(car), rideIds=' '.join(str(id) for id in car)))
+        fp.write(len(slideshow))
+        [fp.write(slide) for slide in slideshow]
